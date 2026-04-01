c295930839bbedcca008fe55a75ef31d270d2f93
jooyoul77 <ohjooyoul@gmail.com>
2026-04-01 23:55:29 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: false
+    },
+    title: '노트',
+    icon: path.join(__dirname, 'icon.png')
+  })
+  win.loadFile('index.html')
+  win.setMenuBarVisibility(false)
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
